4716f1db8b99161045dce51b5fa27cb5df4f3e68
Lady Codington <myemailforcoding@gmail.com>
2016-04-22 21:37:39 -0400

    changed filename to singular

---
+++ spec/models/deal_spec.rb
@@ -0,0 +1,12 @@
+describe 'Deal Attribute Testing' do
+  describe 'A new deal has title, description and price' do
+    subject { Deal.new(title: "5 mani pedi spa package",
+                    description: "Green Tea Spa Special with Hibiscus",
+                    price: 100.00) }
+
+    it { is_expected.to have_attributes(title: "5 mani pedi spa package") }
+    it { is_expected.to have_attributes(description: "Green Tea Spa Special with Hibiscus") }
+    it { is_expected.to have_attributes(price: 100.00) }
+
+  end
+end
