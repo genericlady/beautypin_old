8330bf0801715126446d24c737a27df86548afeb
Lady Codington <myemailforcoding@gmail.com>
2016-04-23 14:38:56 -0400

    devise sign in feature is green

--- spec/features/sing_in_spec.rb
+++ spec/features/sing_in_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Sign in', :devise do
   scenario 'user cannot sign in if not registered' do
     sign_in('person@example.com', 'password')
-    expect(page).to have_content 'Inavlid email or password'
+    expect(page).to have_content 'Invalid email or password.'
   end
 
   scenario 'user can sign in with valid credentials' do
@@ -15,12 +15,12 @@ feature 'Sign in', :devise do
   scenario 'user cannot sign in with the wrong email' do
     user = create(:user)
     sign_in('invalid@email', user.password)
-    expect(page).to have_content 'Email address is invalid'
+    expect(page).to have_content 'Invalid email or password.'
   end
 
   scenario 'user cannt sign in with the wrong password' do
     user = create(:user)
     sign_in(user.email, 'invalidpassword')
-    expect(page).to have_content 'Password is invalid'
+    expect(page).to have_content 'Invalid email or password.'
   end
 end
