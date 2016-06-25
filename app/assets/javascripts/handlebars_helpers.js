04bddb0f9ccf0eaaf0f5bc48838b14a2dd36be2a
genericlady <yiannakokalas@gmail.com>
2016-06-25 10:14:44 -0400

    updated buggy ecma6 standard js

--- app/assets/javascripts/handlebars_helpers.js
+++ app/assets/javascripts/handlebars_helpers.js
@@ -1,14 +1,14 @@
 Handlebars.getTemplate = function(model, filename) {
   if (this.templates === undefined || this.templates[name] === undefined) {
     $.ajax({
-      url : 'app/views/' + model + '/' + filename + '.hbs.erb',
-      success : function(data) {
+      url: 'app/views/' + model + '/' + filename + '.hbs.erb',
+      success: function(data) {
         if (this.templates === undefined) {
           this.templates = {};
         }
         this.templates[name] = this.compile(data);
       },
-      async : false
+      async: false
     });
   }
   return this.templates[name];
