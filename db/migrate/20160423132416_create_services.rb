c2600760f59a48be3a52122e15242cf5fe2366c7
genericlady <yiannakokalas@gmail.com>
2016-06-25 20:01:14 -0400

    updated schema

--- db/migrate/20160423132416_create_services.rb
+++ db/migrate/20160423132416_create_services.rb
@@ -2,7 +2,7 @@ class CreateServices < ActiveRecord::Migration
   def change
     create_table :services do |t|
       t.string :title
-      t.decimal :price, precision: 5, scale: 2
+      # t.decimal :price, precision: 5, scale: 2
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -44,9 +44,9 @@ ActiveRecord::Schema.define(version: 20160506145515) do
   create_table "deals", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.integer  "discount",        limit: 100
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.integer  "discount"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "beauty_place_id"
   end
 
@@ -104,9 +104,8 @@ ActiveRecord::Schema.define(version: 20160506145515) do
 
   create_table "services", force: :cascade do |t|
     t.string   "title"
-    t.decimal  "price",      precision: 5, scale: 2
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "deal_id"
   end
 
