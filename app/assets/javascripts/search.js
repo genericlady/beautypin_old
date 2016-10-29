0af1c1db033f342e2725832793fd1a7a29ed9b6f
genericlady <yiannakokalas@gmail.com>
2016-10-29 15:29:23 -0400

    removed old render functions in replace of handlebars partials

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,31 +3,17 @@ $(function() {
 });
 
 function searchListeners() {
-  $('button.search').click(function(event) {
+  $('button.search-submit').click(function(event) {
+
     var searchCriteria = $('#lastSearch').val();
+
     $.get('/deals.json', { search: searchCriteria })
       .done(function(response) {
-        var deals = deserializeDeals(response);
-        renderDealsTableHTML(deals);
+				clearHTML($('#main'));
+				renderDealsIndex(response);
       });
+
     event.preventDefault;
   });
 }
 
-function deserializeDeals(response) {
-  var serializedDeals = response['deals'];
-  var deserializedDeals = [];
-  for (var i = 0; i < serializedDeals.length; i++) {
-    var newDeal = new Deal(deals[i]); 
-    newDeal.beauty_place = new BeautyPlace(newDeal.beauty_place);
-  }
-  return deals;
-}
-
-function renderDealsTableHTML(deals) {
-  var childrenElements = '';
-  for (var i=0; i < deals.length; i++) {
-    childrenElements += deals[i].renderTR();
-  }
-  $('#dealsTableBody').html(childrenElements);
-}
